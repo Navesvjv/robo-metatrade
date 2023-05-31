88ff99a3655bf3810289f4117540bce50eb5ef50
Joao Naves <srjnaves@gmail.com>
2023-05-31 00:14:54 -0300

    .

--- src/trades/orders.py
+++ src/trades/orders.py
@@ -1,33 +1,39 @@
 import env as env
 import MetaTrader5 as mt5
 from config.singleton import Singleton
+from src.trades.trade_time import TradeTime
 
 
 class Orders(Singleton):
-    def openMarketBuy(self):
-        result = mt5.order_send(
-            symbol=env.symbol,
-            action=mt5.ORDER_ACTION_BUY,
-            volume=env.volume,
-            type=mt5.ORDER_TYPE_MARKET,
-            price=0,
-        )
+    def __init__(self):
+        self.tradeTime = TradeTime()
 
-        if result.retcode != mt5.TRADE_RETCODE_DONE:
-            print(f"Error opening order: {result.comment} ❌")
-        else:
-            print("Order MarketBuy opened successfully! ✅")
+    def openMarketBuy(self):
+        if self.tradeTime.isTradingTime:
+            result = mt5.order_send(
+                symbol=env.symbol,
+                action=mt5.ORDER_ACTION_BUY,
+                volume=env.volume,
+                type=mt5.ORDER_TYPE_MARKET,
+                price=0,
+            )
+
+            if result.retcode != mt5.TRADE_RETCODE_DONE:
+                print(f"Error opening order: {result.comment} ❌")
+            else:
+                print("Order MarketBuy opened successfully! ✅")
 
     def openMarketSell(self):
-        result = mt5.order_send(
-            symbol=env.symbol,
-            action=mt5.ORDER_ACTION_SELL,
-            volume=env.volume,
-            type=mt5.ORDER_TYPE_MARKET,
-            price=0,
-        )
+        if self.tradeTime.isTradingTime:
+            result = mt5.order_send(
+                symbol=env.symbol,
+                action=mt5.ORDER_ACTION_SELL,
+                volume=env.volume,
+                type=mt5.ORDER_TYPE_MARKET,
+                price=0,
+            )
 
-        if result.retcode != mt5.TRADE_RETCODE_DONE:
-            print(f"Error opening order: {result.comment} ❌")
-        else:
-            print("Order MarketSell opened successfully! ✅")
+            if result.retcode != mt5.TRADE_RETCODE_DONE:
+                print(f"Error opening order: {result.comment} ❌")
+            else:
+                print("Order MarketSell opened successfully! ✅")
