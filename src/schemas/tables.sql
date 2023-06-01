344ea58270068cdb401e77b935761e2226a4dcd3
Joao Victor <srjnaves@gmail.com>
2023-05-31 21:16:00 -0300

    .

--- src/schemas/tables.sql
+++ src/schemas/tables.sql
@@ -7,3 +7,14 @@ CREATE TABLE `ticks_win_1min` (
   `ask` decimal(10,5) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `ticks_win_1min` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `symbol` varchar(10) NOT NULL,
+  `time` varchar(30) NOT NULL,
+  `volume` int NOT NULL,
+  `bid` decimal(15,5) NOT NULL,
+  `ask` decimal(15,5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
