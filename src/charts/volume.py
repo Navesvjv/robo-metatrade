1b951d9a1539c8e34cef8b9ddc1c6960b47b3589
Joao Victor <srjnaves@gmail.com>
2023-05-31 22:01:43 -0300

    .

--- src/charts/volume.py
+++ src/charts/volume.py
@@ -3,27 +3,27 @@ import matplotlib.pyplot as plt
 
 
 class VolumeChart:
-    def plot():
-        categorias = ["Compra", "Venda"]
-        valores = [10, 15]
+    def plot(self):
+        prices = ["110", "109", "108", "107", "106"]
+        volumes = [10, 15, 11, 13, 10]
 
         # Configurar o gráfico inicial
         fig, ax = plt.subplots()
-        bar_plot = ax.bar(categorias, valores)
+        bar_plot = ax.bar(prices, volumes)
 
         # Definir as propriedades do gráfico
-        plt.xlabel("Categorias")
-        plt.ylabel("Valores")
+        plt.xlabel("volumes")
+        plt.ylabel("prices")
         plt.title("Gráfico de Barras Vertical")
 
         # Atualizar os valores em tempo real
         while True:
             # Gerar novos valores aleatórios
-            novos_valores = [random.randint(5, 20) for _ in range(len(valores))]
+            novos_valores = [random.randint(5, 20) for _ in range(len(volumes))]
 
             # Atualizar os valores do gráfico de barras
             for bar, novo_valor in zip(bar_plot, novos_valores):
-                bar.set_height(novo_valor)
+                bar.set_width(novo_valor)
 
             # Atualizar o gráfico
             plt.draw()
