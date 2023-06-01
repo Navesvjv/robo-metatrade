185dceaf77e8bef24aa6d6d2eee2f6f7169a3172
Joao Naves <srjnaves@gmail.com>
2023-05-31 22:20:46 -0300

    .

--- index.py
+++ index.py
@@ -2,6 +2,19 @@ from src.config.mt5 import Metatrader
 from src.recordings.tick_recorder import TickRecorder
 from src.testes.print_orders import PrintOrders
 from src.charts.volume import VolumeChart
+from src.charts.chart import BarChart
 
-chart = VolumeChart()
-chart.plot()
+
+data = [
+    {"type": 1, "price": 108890.0, "volume": 578},
+    {"type": 1, "price": 108880.0, "volume": 842},
+    {"type": 1, "price": 108870.0, "volume": 243},
+    {"type": 2, "price": 108860.0, "volume": 153},
+    {"type": 2, "price": 108850.0, "volume": 453},
+    {"type": 2, "price": 108840.0, "volume": 244},
+]
+
+categories = [item["price"] for item in data]
+chart = BarChart(categories)
+
+chart.update_values(data)
