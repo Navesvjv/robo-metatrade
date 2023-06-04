7462e519ba7378642b641a41745b3d776020319d
Joao Naves <srjnaves@gmail.com>
2023-06-04 16:10:11 -0300

    .

--- src/schemas/tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `ticks_win_1min` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `symbol` varchar(10) NOT NULL,
-  `time` varchar(30) NOT NULL,
-  `volume` int NOT NULL,
-  `bid` decimal(15,5) NOT NULL,
-  `ask` decimal(15,5) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
