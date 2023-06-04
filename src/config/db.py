d4185b1c9bdd9a72ab425f9ddc0bf5f2bebf3393
Joao Victor <srjnaves@gmail.com>
2023-06-04 15:33:50 -0300

    .

--- src/config/db.py
+++ src/config/db.py
@@ -1,17 +1,24 @@
 import env
 import mysql.connector
-from .singleton import Singleton
 
 
-class DatabaseConnection(Singleton):
+class DatabaseConnection:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
     def __init__(self):
         self.host = env.db_host
         self.user = env.db_user
         self.password = env.db_pass
         self.database = env.db_name
         self.connection = None
+        self.connected = None
 
-        if not self.connection:
+        if self.connected == None:
             self.connect()
 
     def exec(self, query, values):
@@ -29,11 +36,13 @@ class DatabaseConnection(Singleton):
         )
 
         if self.connection.is_connected():
+            self.connected = True
             print(f"Connected database! ✅")
         else:
             raise Exception("Database connection not established! ❌")
+        
 
     def disconnect(self):
-        if self.connection:
+        if self.connected:
             self.connection.close()
             print("Closed database! ✅")
--- src/config/singleton.py
+++ src/config/singleton.py
@@ -3,6 +3,5 @@ class Singleton:
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            print("New instanceee")
             cls._instance = super().__new__(cls, *args, **kwargs)
         return cls._instance
